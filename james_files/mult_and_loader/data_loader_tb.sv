0bbb9eb19d18e7c6f710bd1e3191fb04d3415c7d
jamesdolle <jdolle@umass.edu>
2024-04-13 18:38:50 -0400

    functional data loader and multiplier

---
+++ james_files/mult_and_loader/data_loader_tb.sv
@@ -0,0 +1,94 @@
+`timescale 1 ns / 1 ps
+
+module data_loader_tb();
+
+    reg CLK, ctrl_logic;
+    reg [3:0] data_send;
+    integer count;
+    wire S0;
+
+    loader loader(CLK, data_send, ctrl_logic);
+    initial begin
+
+        $dumpfile("waveform.vcd"); // Specifies the name of the dump file
+        $dumpvars(0, data_loader_tb);   // Dumps all variables in the module "testbench" and its submodules
+        //$dumpvars(1, data_loader_tb.x1); // For a specific submodule
+        //$dumpvars(0, matrix);   // Dumps all variables in the module "testbench" and its submodules
+        CLK = 0;
+        count = 0;
+        data_send = 2;
+        ctrl_logic = 1;
+        forever #1 CLK = ~CLK;
+    end
+
+    //always #5 CLK=~CLK; //clock toggle every 0.5 ns
+
+    always @(posedge CLK) begin
+        case(count)
+            4'h0: begin
+                data_send <= 2;
+                ctrl_logic <= 1;
+            end
+            4'h1: begin
+                data_send <= 2;
+                ctrl_logic <= 1;
+            end
+            4'h2: begin
+                data_send <= 2;
+                ctrl_logic <= 1;
+            end
+            4'h3: begin
+                data_send <= 2;
+                ctrl_logic <= 1;
+            end
+            4'h4: begin
+                data_send <= 1;
+                ctrl_logic <= 0;
+            end
+            4'h5: begin
+                data_send <= 15;
+                ctrl_logic <= 0;
+            end
+            4'h6: begin
+                data_send <= 2;
+                ctrl_logic <= 0;
+            end
+            4'h7: begin
+                data_send <= 2;
+                ctrl_logic <= 0;
+            end
+            4'h8: begin
+                data_send <= 1;
+                ctrl_logic <= 0;
+            end
+            4'h9: begin
+                data_send <= 7;
+                ctrl_logic <= 0;
+            end
+            4'hA: begin
+                data_send <= 2;
+                ctrl_logic <= 0;
+            end
+            4'hB: begin
+                data_send <= 2;
+                ctrl_logic <= 0;
+            end
+            4'hC: begin
+                #5
+                $display("matrix loaded");
+                $display(loader.matrix_1[0], loader.matrix_1[1]);
+                $display(loader.matrix_1[2], loader.matrix_1[3]);
+                $display("matrix loaded");
+                $display(loader.matrix_2[0], loader.matrix_2[1]);
+                $display(loader.matrix_2[2], loader.matrix_2[3]);
+                $display("size");
+                $display("R1: ", loader.R1, " C1: ", loader.C1);
+                $display("R2: ", loader.R2, " C2: ", loader.C2);
+                //$finish;
+            end
+        endcase
+        count++;
+    end
+
+
+endmodule
---
+++ james_files/mult_and_loader/matrix.sv
@@ -0,0 +1,70 @@
+module matrix(
+    input wire CLK,
+    input reg [31:0] flat_matrix_1 [0:4],
+    input reg [31:0] flat_matrix_2 [0:4],
+    input reg [3:0] R1,
+    input reg [3:0] C1,
+    input reg [3:0] R2,
+    input reg [3:0] C2,
+    input wire readybit
+
+);
+
+    parameter X_DIM = 2;
+    parameter Y_DIM = 2;
+
+    reg [127:0] matrix_1 [0:Y_DIM][0:X_DIM];
+    reg [127:0] matrix_2 [0:Y_DIM][0:X_DIM];
+    reg [127:0] rslt [0:Y_DIM][0:X_DIM];
+
+
+
+    integer x, y, z;
+    integer i, j, k;
+    integer index;
+
+    always @(posedge CLK) begin
+
+
+        if(readybit) begin
+            index = 0;
+            for(i = 0; i < R1; i++) begin
+                for(j = 0; j < C1; j++) begin
+                    matrix_1[i][j] = flat_matrix_1[index++];
+                end
+            end
+
+            index = 0;
+            for(i = 0; i < R2; i++) begin
+                for(j = 0; j < C2; j++) begin
+                    matrix_2[i][j] = flat_matrix_2[index++];
+                end
+            end
+
+
+            $display("matrix unflattened");
+            $display(matrix_1[0][0], matrix_1[0][1]);
+            $display(matrix_1[1][0], matrix_1[1][1]);
+            $display("matrix unflattened");
+            $display(matrix_2[0][0], matrix_2[0][1]);
+            $display(matrix_2[1][0], matrix_2[1][1]);
+
+            $display(" ---- Result ----");
+
+            for (int i = 0; i < X_DIM; i++) begin
+                for (int j = 0; j < Y_DIM; j++) begin
+                    rslt[i][j] = 0;
+        
+                    for (int k = 0; k < X_DIM; k++) begin
+                        rslt[i][j] += matrix_1[i][k] * matrix_2[k][j];
+                    end
+        
+                    $write(rslt[i][j]);
+                end
+                $write("\n");
+            end
+                $finish;  //how long to run before stopping   
+        end
+    end
+
+endmodule
