cc7533ae569f4e68bb355c070d7055631f29a8c5
lestergetHub <144376445+lestergetHub@users.noreply.github.com>
2024-04-17 22:53:12 -0400

    sim file

---
+++ ryan_files/parallel_multiplier/sim/runsim.tcl
@@ -0,0 +1,35 @@
+
+# launch ModelSim in command line mode (vsim -c) and then source this
+# script to run simulation
+
+set RTL_DIR ../rtl
+set SIM_DIR .
+set SYNTH_DIR ../synth/rpt
+
+#create new project in current directory
+project new $SIM_DIR name
+
+# the project source files
+project addfile $RTL_DIR/matrix.v
+project addfile $RTL_DIR/loader.v
+project addfile $SYNTH_DIR/netlist.v
+project addfile $RTL_DIR/testbench.v
+
+
+# add the post-synth tile to project and add the std cells that are
+# needed when simulating the post-synthesis tile 
+
+project addfile $RTL_DIR/slow_vdd1v0_basicCells.v
+
+# compile, load simulation, and then run sim to the end
+project compileall
+
+vsim work.testbench
+add wave -r *
+
+run -all
+quit -sim
+
+# close out the project
+project close
+exit
